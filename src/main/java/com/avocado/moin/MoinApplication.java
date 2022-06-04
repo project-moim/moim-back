65865916b8165aac375dfe96b5eaa381aa5f63bc
Deunsol Park <deunsol916@gmail.com>
2022-06-04 16:31:22 +0900

    FEAT: first commit

---
+++ src/main/java/com/avocado/moin/MoinApplication.java
@@ -0,0 +1,13 @@
+package com.avocado.moin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avocado/moin/MoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avocado.moin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
